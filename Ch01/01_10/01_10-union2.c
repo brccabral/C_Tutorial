211a5755c1bfbf23906e1c0c524f056a9a7a43f8
Bruno Cabral <cabral.brc@gmail.com>
2022-06-07 11:58:14 -0700

    union member changes values of another member, don't use it

--- Ch01/01_10/01_10-union2.c
+++ Ch01/01_10/01_10-union2.c
@@ -12,6 +12,10 @@ int main()
 	printf("reg.a stores %c\n", reg.a);
 
 	reg.b = 1346;
+	printf("reg.a stores %c\n", reg.a);
+	reg.b = 1401;
+	printf("reg.a stores %c\n", reg.a);
+	reg.b = 1349;
 	printf("reg.b stores %d\n", reg.b);
 	printf("reg.a stores %c\n", reg.a);
 
