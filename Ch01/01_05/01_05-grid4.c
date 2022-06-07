cd781c54e110442b0dc139e33a529549fd24bafe
Bruno Cabral <cabral.brc@gmail.com>
2022-06-07 11:03:51 -0700

    multi dimension array as single dimension

--- Ch01/01_05/01_05-grid4.c
+++ Ch01/01_05/01_05-grid4.c
@@ -6,8 +6,19 @@ int main()
 	int x, y, z;
 
 	/* initialize */
-	for (x = 0; x < 27; x++)
-		tictactoe[x] = '.';
+	// for (x = 0; x < 27; x++)
+	// 	tictactoe[x] = '.';
+	for (x = 0; x < 3; x++)
+	{
+		for (y = 0; y < 3; y++)
+		{
+			for (z = 0; z < 3; z++)
+			{
+				// tictactoe[x * 9 + y * 3 + z] = '.';
+				tictactoe[x + y + z] = '.';
+			}
+		}
+	}
 
 	/* display */
 	puts("Tic Tac Toe");
@@ -17,8 +28,9 @@ int main()
 		for (y = 0; y < 3; y++)
 		{
 			for (z = 0; z < 3; z++)
-				printf(" %c ", tictactoe[x * 9 + y * 3 + z]); /* math required to accurately plot out elements */
-			printf("     ");								  /* 5 spaces */
+				// printf(" %c ", tictactoe[x * 9 + y * 3 + z]); /* math required to accurately plot out elements */
+				printf(" %c ", tictactoe[x + y + z]); /* math required to accurately plot out elements */
+			printf("     ");						  /* 5 spaces */
 		}
 		putchar('\n');
 	}
