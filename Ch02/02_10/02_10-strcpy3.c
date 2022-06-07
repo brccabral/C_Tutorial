4f6bfb5f669c9316cdb0b09cc2fd3e4e8392a816
Bruno Cabral <cabral.brc@gmail.com>
2022-06-07 16:24:15 -0700

    string copy

--- Ch02/02_10/02_10-strcpy3.c
+++ Ch02/02_10/02_10-strcpy3.c
@@ -4,11 +4,15 @@
 int main()
 {
 	char original[] = "Am I the original?";
-	char duplicate[] = "Overwritten";
+	char duplicate[1];
 
+	// when the placeholder for the duplicate is shorter
+	// than original, then there is overflow and different
+	// results can occur. In my case, the duplicate
+	// was fine, but the original was not
 	printf("Here is the original string: %s\n", original);
 	strcpy(duplicate, original);
-	printf("Here is the duplicate: %s\n", duplicate);
+	printf("Here is the duplicate: %s original %s\n", duplicate, original);
 
 	return (0);
 }
