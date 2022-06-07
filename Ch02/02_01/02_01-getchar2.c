6157193a8a4198d85a382ea74feb6ade77b7483a
Bruno Cabral <cabral.brc@gmail.com>
2022-06-07 12:23:04 -0700

    input characters

--- Ch02/02_01/02_01-getchar2.c
+++ Ch02/02_01/02_01-getchar2.c
@@ -2,13 +2,17 @@
 
 int main()
 {
-	int ch1, ch2;
+	int ch1, ch2, ch3, ch4;
 
 	printf("Type a character: ");
 	ch1 = getchar();
 	printf("Type a character: ");
 	ch2 = getchar();
-	printf("Characters '%c' and '%c' received.\n", ch1, ch2);
+	printf("Type a character: ");
+	ch3 = getchar();
+	printf("Type a character: ");
+	ch4 = getchar();
+	printf("Characters '%c', '%c', '%c', '%c' received.\n", ch1, ch2, ch3, ch4);
 
 	return (0);
 }
--- Ch02/02_01/02_01-typewriter1.c
+++ Ch02/02_01/02_01-typewriter1.c
@@ -7,7 +7,7 @@ int main()
 	while (1)
 	{
 		a = getchar();
-		if (a == EOF)
+		if (a == EOF) // EOF = Ctrl + D (done) (Ctrl + C is interruption)
 			break;
 		putchar(a);
 	};
